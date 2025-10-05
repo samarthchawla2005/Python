ea22f1a88a84315c032d7df9e4aea4db5bc644e4
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-05 11:08:19 +0530

    Add files via upload

---
+++ Numerical_Operators.py
@@ -0,0 +1,69 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Numerical Operators
+>>> 
+>>> 9+2*3
+15
+>>> (9+2)*3
+33
+>>> 8*8*8
+512
+>>> 8**3
+512
+>>> 8**10
+1073741824
+>>> 8/2
+4.0
+>>> 8//2
+4
+>>> 9//7
+1
+>>> 9/7
+1.2857142857142858
+>>> 8%2
+0
+>>> 8%%2
+SyntaxError: invalid syntax
+>>> 10 % 3
+1
+>>> 3 % 9
+3
+>>> 9 % 3
+0
+>>> _
+0
+>>> __
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    __
+NameError: name '__' is not defined. Did you mean: '_'?
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+num1 = 8**3
+num1
+512
+print(num1)
+512
+5 + num1
+517
+num2 = 5
+num1 + num2
+517
+mu3 = num1 + num2
+num3 = num1 + num2
+num3
+517
+print(num3)
+517
+num3 = mu3
+?
+SyntaxError: invalid syntax
+num3 == mu3
+True
+num3 != mu3
+False
+(5+5)*2
+20
