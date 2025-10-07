4334c9eae6766ebf634ca3b7ce5460f9aed3f51a
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-07 09:03:19 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,39 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 45
+45
+>>> samarth
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    samarth
+NameError: name 'samarth' is not defined
+>>> num = 8
+>>> num
+8
+>>> name = 'samarth'
+>>> name
+'samarth'
+>>> 'sam'
+'sam'
+>>> print('sam')
+sam
+>>> print("Samarth's Laptop")
+Samarth's Laptop
+>>> print('samarth\'s Laptop')
+samarth's Laptop
+>>> print('Samart\'s "Laptop"')
+Samart's "Laptop"
+>>> 'sam' * 10
+'samsamsamsamsamsamsamsamsamsam'
+>>> 'sam " * 10
+SyntaxError: unterminated string literal (detected at line 1)
+>>> 'sam ' * 10
+'sam sam sam sam sam sam sam sam sam sam '
+>>> 'sam \' ' *10
+"sam ' sam ' sam ' sam ' sam ' sam ' sam ' sam ' sam ' sam ' "
+>>> "'sam \' '" * 10
+"'sam ' ''sam ' ''sam ' ''sam ' ''sam ' ''sam ' ''sam ' ''sam ' ''sam ' ''sam ' '"
+>>> print('C:\\users\\chawl')
+C:\users\chawl
+'samarth' 'chawla'
+
